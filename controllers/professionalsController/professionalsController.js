962c25129a07366a7872bd9db8ffc9655f00a27d
Adriel Amadi <adrielamadi7@gmail.com>
2024-11-26 18:37:57 +0100

    I added functionality for professionals to set schedule for a single day

--- controllers/professionalsController/professionalsController.js
+++ controllers/professionalsController/professionalsController.js
@@ -46,17 +46,18 @@ professionalsController.setSchedule = ("/set-schedule", async (req, res)=>{
         const payload = JSON.parse(req.body)
         const userID = req.decodedToken.userID
 
+        const timeMap = {
+            "12AM": 0, "1AM": 1, "2AM": 2, "3AM": 3 , "4AM": 4, "5AM": 5, "6AM": 6,
+            "7AM": 7, "8AM": 8, "9AM": 9, "10AM": 10, "11AM": 11, "12PM": 12, "1PM": 13,
+            "2PM": 14, "3PM": 15, "4PM": 16, "5PM": 17, "6PM": 18, "7PM": 19, "8PM": 20,
+            "9PM": 21, "10PM": 22, "11PM": 23
+        }
+
         if(payload.scheduleType ==  "range"){
             //Break down data
             const schedules = []
             const startDayInt = parseInt(payload.startDay)
             const endDayInt = parseInt(payload.endDay)
-            const timeMap = {
-                "12AM": 0, "1AM": 1, "2AM": 2, "3AM": 3 , "4AM": 4, "5AM": 5, "6AM": 6,
-                "7AM": 7, "8AM": 8, "9AM": 9, "10AM": 10, "11AM": 11, "12PM": 12, "1PM": 13,
-                "2PM": 14, "3PM": 15, "4PM": 16, "5PM": 17, "6PM": 18, "7PM": 19, "8PM": 20,
-                "9PM": 21, "10PM": 22, "11PM": 23
-            }
            
             const startTimeInt = timeMap[`${payload.startTime}${payload.startMeridian}`]
             
@@ -72,9 +73,13 @@ professionalsController.setSchedule = ("/set-schedule", async (req, res)=>{
                     schedule.day = d
                     schedule.startTime = t;
                     schedule.endTime = t + 1;
-                    // schedule.startMeridian = payload.startMeridian
-                    // schedule.endMeridian = payload.endMeridian
-
+                    
+                    //check if this schedule already exists
+                    const existingSchedule = await database.findOne({year: schedule.year, month: schedule.month, day: schedule.day, startTime: schedule.startTime, endTime: schedule.endTime}, database.collection.schedules)
+                    if(existingSchedule){
+                        utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, responseData: {msg: "schedule already exists"}}, true)
+                        return
+                    }
                     schedules.push(schedule)
                 
                 }
@@ -85,6 +90,37 @@ professionalsController.setSchedule = ("/set-schedule", async (req, res)=>{
         
             utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, responseData: {msg: "success"}}, true)
 
+        }
+        if(payload.scheduleType ==  "single"){
+            //Break down data
+            const schedules = []
+            const dayInt = parseInt(payload.day)
+            const startTimeInt = timeMap[`${payload.startTime}${payload.startMeridian}`]
+            const endTimeInt = timeMap[`${payload.endTime}${payload.endMeridian}`]
+
+            for(let t = startTimeInt; t <endTimeInt; t++){  
+                const date = new Date();
+                const year = date.getFullYear()
+                const schedule = {professional: ObjectId.createFromHexString(userID), booked: false};
+                schedule.year = year
+                schedule.month = payload.month
+                schedule.day = dayInt
+                schedule.startTime = t;
+                schedule.endTime = t + 1;
+                
+                //check if this schedule already exists
+                const existingSchedule = await database.findOne({year: schedule.year, month: schedule.month, day: schedule.day, startTime: schedule.startTime, endTime: schedule.endTime}, database.collection.schedules)
+                if(existingSchedule){
+                    utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, responseData: {msg: "schedule already exists"}}, true)
+                    return
+                }
+                schedules.push(schedule)
+            
+            }
+
+            await database.insertMany(schedules, database.collection.schedules)
+        
+            utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, responseData: {msg: "success"}}, true)
         }
         
 
